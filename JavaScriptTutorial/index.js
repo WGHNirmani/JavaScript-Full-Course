59a6db3af712895fe84b1c0705302c2416ec5a50
Hirunika Nirmani <hirunikanirmani26@gmail.com>
2024-10-21 21:37:53 +0530

    Add files via upload

---
+++ JavaScriptTutorial/index.js
@@ -0,0 +1,17 @@
+// console.log('Hello');
+// console.log('I like Pizza!');
+
+// window.alert('This is an alert!');
+// window.alert('I like Pizza!');
+
+// This is a comment -> single line comment
+
+/*
+    This
+    is 
+    a
+    comment -> multi line comment
+*/
+
+document.getElementById("myH1").textContent = 'Hello';
+document.getElementById("myP").textContent = "I like pizza!";
