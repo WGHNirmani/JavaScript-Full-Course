d3371388352b665c36f29edf9d115f0b7b0c2726
Hirunika Nirmani <hirunikanirmani26@gmail.com>
2024-10-21 21:48:12 +0530

    Create index.js

---
+++ Variables/index.js
@@ -0,0 +1,22 @@
+// variable = A container that stores a value.
+//            Behaves as if it were the value it contains.
+
+// 1. declaration let x;
+// 2. assignment x = 100;
+
+// let x;
+// x = 123;
+
+// let x = 123;
+
+// console.log(x);
+
+let age = 25;
+console.log(age);
+console.log(`You are ${age} years old`);
+
+let price = 10.99;
+console.log(price);
+
+let gpa = 2.1;
+console.log(gpa);
