d85695fc9f6a34510ff20d06ce83e5579e4dded0
Hirunika Nirmani <hirunikanirmani26@gmail.com>
2024-10-22 11:26:54 +0530

    Add files via upload

---
+++ 2.Variables/index.js
@@ -0,0 +1,53 @@
+// variable = A container that stores a value.
+//            Behaves as if it were the value it contains.
+
+// 1. declaration let x;
+// 2. assignment x = 100;
+
+// let x;
+// x = 123;
+
+// let x = 123;
+
+// console.log(x);
+
+// let age = 25;
+// console.log(typeof age)
+// console.log(age);
+// console.log(`You are ${age} years old`);
+
+let price = 10.99;
+console.log(price);
+console.log(`The price is $${price}`);
+
+let gpa = 2.1;
+console.log(gpa);
+console.log(`Your gpa is ${gpa}`);
+
+let firstName = "Hirunika";
+console.log(typeof firstName);
+console.log(`Your name is ${firstName}`);
+
+let favoriteFood = "Pizza";
+console.log(`You like ${favoriteFood}`);
+
+let email = "hirunikanirmani26@gmail.com";
+console.log(`Your email is ${email}`);
+
+let online = false;
+console.log(`Hirunika is online: ${online}`);
+
+let forSale = true;
+console.log(`Is this car for sale: ${forSale}`);
+
+// let isStudent = true;
+// console.log(`Enrolled: ${isStudent}`);
+
+let fullName = "Hirunika Nirmani";
+let age = 25;
+let isStudent = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+
